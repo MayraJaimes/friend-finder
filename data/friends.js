24eec8596fcde6f52370be39ba419e38144de5ee
Mayra Jaimes <mtj26@georgetown.edu>
2018-03-02 11:15:13 -0600

    changed my code to make it clearer

--- data/friends.js
+++ data/friends.js
@@ -1,71 +1,62 @@
-var users= [{
-    "name":"Mayra",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[
-        1,
-        1,
-        1,
-        1
-      ]
-  },
-  {
-    "name":"Ahmed",
-    "photo":"http:linkforAhmed",
-    "scores":[
-        4,
-        5,
-        6,
-        3
-      ]
-  },
-  {
-    "name":"dennis",
-    "photo":"http:linkforDennis",
-    "scores":[
-        1,
-        2,
-        3,
-        4
-      ]
-  }];
-
-var tempUsers = [];
-var tempScores = [];
-
-function score(arr){
-    var result;
-    for (var i = 1; i < arr.length; i++) {
-        currentUser = arr[0];
-        otherUser = arr[i];
-        tempUsers.push([totalDifference(currentUser, otherUser), otherUser.name, otherUser.photo]);
-        tempScores.push(totalDifference(currentUser, otherUser));
+const users = [
+    {
+      name: "Mayra",
+      photo:
+        "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+      scores: [1, 1, 1, 1]
+    },
+    {
+      name: "Ahmed",
+      photo: "http:linkforAhmed",
+      scores: [1, 1, 1, 1]
+    },
+    {
+      name: "dennis",
+      photo: "http:linkforDennis",
+      scores: [1, 2, 3, 4]
+    },
+    {
+      name: "jon",
+      photo: "http:linkforJon",
+      scores: [1, 2, 2, 1]
     }
-}
-
-function totalDifference (currUser, users) {
-    var tempArray= [];
-    var result;
-    for (var i = 0; i < users.scores.length; i++) {
-        var difference = currUser.scores[i] - users.scores[i];
-        var totalDifference = Math.abs(difference);
-        tempArray.push(totalDifference);
+  ];
+  
+  const findTotalScore = usersArr => {
+    let [currentUser, ...otherUsers] = usersArr;
+    let bestMatches = [];
+    let lowestCompatibility = 1000;
+  
+    otherUsers.forEach((otherUser) => {
+        let compatibility = totalDifference(currentUser.scores, otherUser.scores);
+        otherUser.compatibility = compatibility;
+  
+        if (lowestCompatibility > compatibility) {
+            lowestCompatibility = compatibility;
+            bestMatches = [];
+            bestMatches.push(otherUser);
+      } else if (lowestCompatibility === compatibility) {
+            lowestCompatibility = compatibility;
+            bestMatches.push(otherUser);
+      }
+    });
+      return bestMatches;
+  }
+  
+  
+  function totalDifference(currScores, otherUserScores) {
+    var tempArray = [];
+  
+    for (var i = 0; i < otherUserScores.length; i++) {
+      var difference = Math.abs(currScores[i] - otherUserScores[i]);
+      tempArray.push(difference);
     }
-    result = totalSum(tempArray);
-    return result;
-}
-
-function totalSum(arr) {
-    return arr.reduce((total, num) => total + num);
-}
-
-function bestMatch(arr) {
-    var min = Math.min.apply(null, tempScores);
-    for (var i = 0; i < tempUsers.length; i++) {
-        if (tempUsers[i][0] === min){
-            console.log(tempUsers[i][1], tempUsers[i][2]);
-        }
-    }
-}
-
-score(users);
-bestMatch(tempUsers);
+  
+    return tempArray.reduce((total, num) => total + num);
+  }
+  
+  const bestMatch = findTotalScore(users);
+  
+  console.log('bestMatch', bestMatch);
+  //console.log(`Best match is ${bestMatch.name}!`);
+  
