64a201248d69d27cf83426ad8af940945a4c80b3
Mayra Jaimes <mtj26@georgetown.edu>
2018-03-01 21:16:39 -0600

    have a function that is able to give me the user name and photo of the best match/matches

--- data/friends.js
+++ data/friends.js
@@ -1,25 +1,16 @@
-
-// var users= [[7, 6, 2], [4, 3, 2], [4, 5, 1], [7, 6, 2], [6, 5, 1]];
-
-// users[0]
-// users[0][i];
-
-var temp = [];
-
-
 var users= [{
     "name":"Mayra",
     "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
     "scores":[
-        7,
-        8,
-        9,
-        2
+        1,
+        1,
+        1,
+        1
       ]
   },
   {
     "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "photo":"http:linkforAhmed",
     "scores":[
         4,
         5,
@@ -29,7 +20,7 @@ var users= [{
   },
   {
     "name":"dennis",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "photo":"http:linkforDennis",
     "scores":[
         1,
         2,
@@ -38,47 +29,43 @@ var users= [{
       ]
   }];
 
+var tempUsers = [];
+var tempScores = [];
 
-function score(arr) {
-    function getSum(total, num) {
-        return total + num;
-    }
-    return arr.reduce(getSum);
-}
-
-var currentUser;
-var otherUser;
-
-function goingThrough(arr){
+function score(arr){
     var result;
     for (var i = 1; i < arr.length; i++) {
         currentUser = arr[0];
         otherUser = arr[i];
-        temp.push([otherUser.name, totalDifference(currentUser, otherUser)]);
+        tempUsers.push([totalDifference(currentUser, otherUser), otherUser.name, otherUser.photo]);
+        tempScores.push(totalDifference(currentUser, otherUser));
     }
-    console.log("temp", temp);
 }
 
 function totalDifference (currUser, users) {
     var tempArray= [];
     var result;
-
     for (var i = 0; i < users.scores.length; i++) {
-        console.log(currUser.scores[i], users.scores[i]);
         var difference = currUser.scores[i] - users.scores[i];
         var totalDifference = Math.abs(difference);
         tempArray.push(totalDifference);
     }
-    console.log(tempArray);
-    result = score(tempArray);
-    console.log(result);
+    result = totalSum(tempArray);
     return result;
 }
 
-function score(arr) {
+function totalSum(arr) {
     return arr.reduce((total, num) => total + num);
 }
 
-goingThrough(users);
+function bestMatch(arr) {
+    var min = Math.min.apply(null, tempScores);
+    for (var i = 0; i < tempUsers.length; i++) {
+        if (tempUsers[i][0] === min){
+            console.log(tempUsers[i][1], tempUsers[i][2]);
+        }
+    }
+}
 
-console.log(temp);
+score(users);
+bestMatch(tempUsers);
