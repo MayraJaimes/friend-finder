64169009e76e54fd772f01a3659197231959b133
Mayra Jaimes <mtj26@georgetown.edu>
2018-03-02 17:26:43 -0600

    added the function to find the best match in the api routes page

--- data/friends.js
+++ data/friends.js
@@ -1,69 +1,21 @@
-const users = [
+var users = [
     {
       name: "Mayra",
-      photo:
-        "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+      photo: "http:linkforMayra",
       scores: [1, 1, 1, 1]
     },
     {
-      name: "Ahmed",
-      photo: "http:linkforAhmed",
-      scores: [1, 2, 2, 1]
-    },
-    {
       name: "dennis",
-      photo: "http:linkforDennis",
-      scores: [1, 2, 3, 4]
+      photo: "http:linkforMayra",
+      scores: [2, 3, 2, 2]
     },
     {
-      name: "jon",
-      photo: "http:linkforJon",
-      scores: [1, 2, 2, 1]
+      name: "Jon",
+      photo: "http:linkforMayra",
+      scores: [2, 3, 2, 2]
     }
   ];
-
 
   module.exports = users;
 
   
-  // const findTotalScore = usersArr => {
-  //   let [currentUser, ...otherUsers] = usersArr;
-  //   let bestMatches = [];
-  //   let lowestCompatibility = 1000;
-  
-  //   otherUsers.forEach((otherUser) => {
-  //       let compatibility = totalDifference(currentUser.scores, otherUser.scores);
-  //       otherUser.compatibility = compatibility;
-  
-  //       if (lowestCompatibility > compatibility) {
-  //           lowestCompatibility = compatibility;
-  //           bestMatches = [];
-  //           bestMatches.push(otherUser);
-  //     } else if (lowestCompatibility === compatibility) {
-  //           lowestCompatibility = compatibility;
-  //           bestMatches.push(otherUser);
-  //     }
-  //   });
-  //     return bestMatches;
-  // }
-  
-  // function totalDifference(currScores, otherUserScores) {
-  //   var tempArray = [];
-  
-  //   for (var i = 0; i < otherUserScores.length; i++) {
-  //     var difference = Math.abs(currScores[i] - otherUserScores[i]);
-  //     tempArray.push(difference);
-  //   }
-  //   return tempArray.reduce((total, num) => total + num);
-  // }
-  
-  // const bestMatch = findTotalScore(users);
-    
-  // function displayMatchInfo (bestMatch) {
-  //   bestMatch.forEach((userInfo) => {
-  //     console.log(`Best match is ${userInfo.name}!`)
-  //     console.log(userInfo.photo);
-  //   });
-  // }
-
-  // displayMatchInfo(bestMatch);
--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -3,5 +3,59 @@ var friendsData = require("../data/friends");
 module.exports = function(app) {
   app.get("/api/friends", function(req, res) {
     res.json(friendsData);
-  })
-};
+  });
+
+  app.post("/api/friends", function(req, res) {
+    var newuser = req.body;
+    console.log(newuser);
+    friendsData.push(newuser);
+    res.json(newuser);
+  });
+
+    const findTotalScore = friendsData => {
+    let [currentUser, ...otherUsers] = friendsData;
+    console.log(currentUser);
+    let bestMatches = [];
+    let lowestCompatibility = 1000;
+  
+    otherUsers.forEach((otherUser) => {
+        let compatibility = totalDifference(currentUser.scores, otherUser.scores);
+        otherUser.compatibility = compatibility;
+  
+        if (lowestCompatibility > compatibility) {
+            lowestCompatibility = compatibility;
+            bestMatches = [];
+            bestMatches.push(otherUser);
+      } else if (lowestCompatibility === compatibility) {
+            lowestCompatibility = compatibility;
+            bestMatches.push(otherUser);
+      }
+    });
+    console.log(bestMatches);
+      return bestMatches;
+  }
+  
+  function totalDifference(currScores, otherUserScores) {
+    var tempArray = [];
+  
+    for (var i = 0; i < otherUserScores.length; i++) {
+      var difference = Math.abs(currScores[i] - otherUserScores[i]);
+      tempArray.push(difference);
+    }
+    return tempArray.reduce((total, num) => total + num);
+  }
+  
+  const bestMatch = findTotalScore(friendsData);
+    
+  function displayMatchInfo (bestMatch) {
+    bestMatch.forEach((userInfo) => {
+      console.log(`Best match is ${userInfo.name}!`)
+      console.log(userInfo.photo);
+    });
+  }
+
+  displayMatchInfo(bestMatch);
+
+    
+}
+
