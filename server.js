eba6f1e408d418b384bddfaccfcb4a2aed77db02
Mayra Jaimes <mtj26@georgetown.edu>
2018-03-03 17:22:03 -0600

    adding styles

--- server.js
+++ server.js
@@ -5,11 +5,10 @@ var PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
+app.use(express.static("public"));
 
 require("./routing/apiRoutes")(app);
 require("./routing/htmlRoutes")(app);
-
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
