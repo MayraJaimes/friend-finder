d89d2045ff8317202bdcb855a3bdddcd168ffe14
Mayra Jaimes <mtj26@georgetown.edu>
2018-03-03 19:48:07 -0600

    installed the npm package path

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
 var PORT = process.env.PORT || 8080;
+var path = require("path");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
